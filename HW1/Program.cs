5214b41c17fa1914060503425379f7b4ef846dee
Rodion23 <rodion23@mail.ru>
2023-01-25 12:00:17 +0700

    Add hw1 correct

---
+++ HW1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+System.Console.Write("Трехзначное число > ");
+string number = Console.ReadLine();
+int num = Convert.ToInt32(number);
+if (num > 99 && num < 1000)
+{
+    int result = (num / 10) % 10;
+    System.Console.WriteLine($"Вторая цифра числа {num} равна {result}");
+}
+else
+    System.Console.WriteLine($"Число не трехзначное");
