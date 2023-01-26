264ad7538fe73fa1db7f92725cd3169c52e1502d
Rodion23 <rodion23@mail.ru>
2023-01-26 11:38:41 +0700

    Add HW2

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -3,12 +3,15 @@
 78 -> третьей цифры нет
 32679 -> 6
 */
-System.Console.Write("ВВедите число > ");
+System.Console.Write("Введите число > ");
 string number = Console.ReadLine();
 int num = Convert.ToInt32(number);
+double len = Convert.ToDouble(number.Length);
+double st = Math.Pow(10, (len - 3));
+int stepen = Convert.ToInt32(st);
 if (num > 99)
 {
-    int result = num % 10;
+    int result = num / stepen % 10;
     System.Console.WriteLine($"Третья цифра числа {num} равна {result}");
 }
 else
