89cb47d1889117e6a3081bffa59b2cbf491cb56f
Rodion23 <rodion23@mail.ru>
2023-01-26 12:02:25 +0700

    Add HW4

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -1,6 +1,9 @@
 ﻿/*Задача 4 * : Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. 
 Не использовать строки для расчета.*/
 
-Random rnd =new Random();
+Random rnd = new Random();
 int number = rnd.Next(100, 999);
 System.Console.WriteLine(number);
+int num1 = (number / 100) % 10;
+int num2 = number % 10;
+System.Console.WriteLine($"Случайное число {number}, число без второй цифры {num1}{num2}");
